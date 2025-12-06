c9daf3ff0dd6a06c18d6c43236938ca21151c0e6
Yura Mishin <robothot@yandex.ru>
2025-12-07 02:17:28 +0300

    Посадил блок с курсами на Vue

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import { Form } from './assets/scripts/components/Form.js'
-import { InputMask } from './assets/scripts/components/InputMask.js'
-import { Select } from './assets/scripts/components/Select.js'
-import { Header } from './assets/scripts/modules/Header.js'
+import { Form } from './assets/scripts/components/Form.js';
+import { InputMask } from './assets/scripts/components/InputMask.js';
+import { Select } from './assets/scripts/components/Select.js';
+
+import coursesJson from './data/courses.json' with { type: "json" };
+
+const { createApp } = Vue;
 
 function main() {
     const namePage = document.querySelector('meta[name="page"]').content
@@ -14,56 +17,6 @@ function main() {
         input: '.callback__wrapper',
         options: '.callback__item'
     })
-
-    // const selectCallback = new Select({
-    //     obj: '.callback__select',
-    // })
-
-    // const header = new Header('.header')
-
-    const header = new Header({
-        elements: {
-            header: ".header",
-            burger: "#burger",
-            searchBtn: ".panel__btn_search",
-            searchInput: ".panel__form",
-            chatBtn: ".header__chat-btn",
-        },
-
-        handlers: {
-            header: {
-                target: 'window',
-                event: "scroll",
-                callback() {
-                    this.onScroll();
-                }
-            },
-            burger: {
-                event: "click",
-                callback() {
-                    this.toggleMenu();
-                },
-            },
-            searchBtn: {
-                event: "click",
-                callback() {
-                    this.toggleSearch();
-                },
-            },
-            searchInput: {
-                event: "input",
-                callback(e) {
-                    this.searchData();
-                },
-            },
-            chatBtn: {
-                event: "click",
-                callback() {
-                    this.handleChat();
-                },
-            },
-        },
-    });
 
     const coursesSlider = new Swiper('.reviews__slider', {
         direction: 'horizontal',
@@ -87,10 +40,69 @@ function main() {
         },
     })
 
-    // particlesJS.load('particles-js', '../../assets/libs/particles/particles.json', function () {
-    //     console.log('callback - particles.js config loaded');
-    // });
 
+    const headerSettings = {
+        data() {
+            return {
+                message: 'Привет Vue!',
+                isOpenSubMenu: false,
+                isOpenSearch: false,
+                isScrolled: false
+            }
+        },
+        methods: {
+            toggleSubMenu() {
+                this.isOpenSubMenu = !this.isOpenSubMenu
+            },
+            toggleSearch() {
+                this.isOpenSearch = !this.isOpenSearch
+            },
+            handleScroll() {
+                this.isScrolled = window.scrollY > 50;
+            }
+        },
+        watch: {
+            isScrolled(newValue) {
+                const headerEl = document.querySelector('.header');
+                if (newValue) {
+                    headerEl.classList.add('scrolled');
+                } else {
+                    headerEl.classList.remove('scrolled');
+                }
+            }
+        },
+        mounted() {
+            window.addEventListener('scroll', this.handleScroll);
+        }
+    }
+
+    const coursesSettings = {
+        data() {
+            return {
+                courses: coursesJson
+            }
+        },
+        methods: {
+            getLabel(label) {
+                switch (label) {
+                    case 'popular':
+                        return 'Популярный'
+                    case 'new':
+                        return 'Новый'
+                    case 'recommendation':
+                        return 'Рекомендуем'
+                    default:
+                        return 'Скоро'
+                }
+            }
+        }
+    }
+
+
+    const headerApp = createApp(headerSettings)
+    const coursesApp = createApp(coursesSettings)
+    headerApp.mount('.header')
+    coursesApp.mount('.courses')
 }
 
 document.addEventListener("DOMContentLoaded", () => {
