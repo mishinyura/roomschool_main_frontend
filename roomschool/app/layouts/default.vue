4a95ef2a3ef47b9ea552ac8af0e9371eb322479d
Yura Mishin <robothot@yandex.ru>
2025-12-16 18:40:53 +0300

    Поправил обсщую разметку

--- roomschool/app/layouts/default.vue
+++ roomschool/app/layouts/default.vue
@@ -3,8 +3,23 @@
     <AppSprite />
     <TheHeader />
 
-    <slot />
+    <main class="page-content">
+      <slot />
+    </main>
 
     <TheFooter />
   </div>
 </template>
+
+<style scoped>
+.main-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Растягиваем на всю высоту окна браузера */
+}
+
+.page-content {
+  flex-grow: 1; /* Заставляет этот блок занимать всё свободное место */
+  /* Теперь футер всегда будет прижат к низу, даже если контента нет */
+}
+</style>
